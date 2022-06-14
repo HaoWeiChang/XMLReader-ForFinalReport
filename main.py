b47dcba097f65997d4548cca6a20aac7c5e4db61
HaoWeiChang <howard3317@gmail.com>
2022-06-14 21:47:07 +0800

    use beautifualsoup func

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
 from typing import List
 from bs4 import BeautifulSoup
-from os import walk 
-import os 
+from os import walk
+import os
+import re
+
 
 def show_file() -> List[str]:
     f = []
-    for (dirpath,_,filenames) in walk(os.getcwd()):
-        for name in filenames: 
+    for (dirpath, _, filenames) in walk(os.getcwd()):
+        for name in filenames:
             if name.endswith(".XML"):
-                f.append(os.path.join(dirpath,name))
-    for i,v in enumerate(f):
+                f.append(os.path.join(dirpath, name))
+    for i, v in enumerate(f):
         print(f'{i}:{v}')
     return f
 
+
 class XmlFile:
     def __init__(self, path: str) -> None:
+        self.name = "".join(path.split('\\')[-1])
         self.path = path
         self.description = ""
         self.keyword = []
         self.combineword = []
         pass
 
-    def read(self) -> None:
-        with open(self.path, "r") as xml:
-            contents = xml.read()
-            text = BeautifulSoup(contents, 'xml')
-            self.description = text.find_all('description')
-        xml.close()
+    def answer_a(self) -> None:
+        contents = open(self.path, "r").read()
+        soup = BeautifulSoup(contents, 'lxml')
+        data_grant = soup.find("us-bibliographic-data-grant")
+        abstract = soup.find("abstract")
+        description = soup.find("description")
+        texts = re.sub('<[^<]+>', "", open(self.path, "r").read())
 
-    def print(self):
+    def answer_b_c(self):
         print(self.description)
 
 
 if __name__ == '__main__':
     f = show_file()
-    a = int(input())
+    a = int(input("input number:"))
     xml = XmlFile(f[a])
-    xml.read()
-    xml.print()
+    xml.answer_a()
+    # xml.answer_b_c()
